b640e2a066499666ea0defb5de2dc619b638fdf6
Joanna Żebrowska <zebrowska2137@gmail.com>
2025-05-21 23:00:26 +0200

    displaying recipes in separeate tiles

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -1,24 +1,19 @@
-window.onload = () => { //kod uruchomi sie dopiero gdy strona calkowicie sie zaladuje (html,css,obrazki etc.)
-  fetch("http://localhost:8080/products") //wysyla zapytanie typu get do backendu springboot pod adresem /products
-    .then((response) => response.json()) //konwercja odp. http do formatu json(czyli obiektu JS)
-    .then((data) => fillProductTable(data));  //po pobraniu danych, przekazuje je do funkcji fillproducttable,ktora:
+window.onload = () => {
+  fetch("http://localhost:8080/products")
+    .then((response) => response.json())
+    .then((data) => fillProductTable(data));
 };
 
 function formatDatePL(dateStr) {
-    if (!dateStr) return "";
-    const[year, month, day] = dateStr.split("-");
-    return `${day}.${month}.${year}`;
+  if (!dateStr) return "";
+  const [year, month, day] = dateStr.split("-");
+  return `${day}.${month}.${year}`;
 }
 
 function fillProductTable(data) {
-  let table = document.querySelector("#product-table tbody"); //zbiera referencje do tabeli w html (tbody)
-
-  data.forEach((x) => { //do kazdego produktu z backendu (x) tworzy wiersz tr i 3 komorki td
+  let table = document.querySelector("#product-table tbody");
+  data.forEach((x) => {
     let newRow = document.createElement("tr");
-
-    // let id = document.createElement("td");
-    // id.textContent = x.id;
-    // newRow.appendChild(id);
 
     let name = document.createElement("td");
     name.textContent = x.name;
@@ -34,35 +29,32 @@ function fillProductTable(data) {
 
     table.appendChild(newRow);
   });
-  //ugulnie dane z backendu leca przez fetch, sa zamieniane na js-owy obiekt i automatycznie wysweitlane jako wiersze tabeli w htlm
 }
 
-document.querySelector(".add-product-form").addEventListener('submit', event => {
-    addProduct(event);
-})
+document.querySelector(".add-product-form").addEventListener("submit", (event) => {
+  addProduct(event);
+});
 
 function addProduct(event) {
-    event.preventDefault();
-    let data = new FormData(event.target);
-    let newProduct = {}
+  event.preventDefault();
+  let data = new FormData(event.target);
+  let newProduct = {};
 
-    data.forEach((value, key) => newProduct[key] = value)
+  data.forEach((value, key) => (newProduct[key] = value));
 
-    fetch("http://localhost:8080/products", {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify([newProduct])
-    })
-    .then(response => {
-        if(response.ok) {
-            location.reload();
-        }
-    })
+  fetch("http://localhost:8080/products", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify([newProduct]),
+  }).then((response) => {
+    if (response.ok) {
+      location.reload();
+    }
+  });
 }
-
 
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("delete-btn")) {
@@ -73,28 +65,35 @@ document.addEventListener("click", function (event) {
 
 function deleteProduct(id) {
   fetch(`http://localhost:8080/products/${id}`, {
-    method: "DELETE"
-  })
-  .then(response => {
+    method: "DELETE",
+  }).then((response) => {
     if (response.ok) {
-      location.reload(); 
+      location.reload();
     } else {
       console.error("Błąd podczas usuwania produktu");
     }
   });
 }
 
-
-
-
+// MODERNIZED RECIPE DISPLAY
 $(document).ready(function () {
   $('#get-recipes').click(function () {
     $.get('http://localhost:8080/recipes', function (response) {
-      $('#recipes-result').html('<pre>' + response.recipes + '</pre>');
+      const recipesContainer = $('#recipes-result');
+      recipesContainer.empty();
+
+      const recipes = response.recipes.split(/\n\d+\.\s/).filter(Boolean);
+
+      recipes.forEach((recipe, index) => {
+        const recipeDiv = $('<div>').addClass('recipe-card');
+        recipeDiv.html(`
+          <h3>Przepis ${index + 1}</h3>
+          <p>${recipe.replace(/-\s/g, "<br>• ")}</p>
+        `);
+        recipesContainer.append(recipeDiv);
+      });
     }).fail(function () {
       alert("Błąd podczas pobierania przepisów");
     });
   });
 });
- 
-
